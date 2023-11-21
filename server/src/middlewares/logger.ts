2e76a321a7777c6dd437f96b73f8071cdce92cec
eliscioter~ <attila433lehun@gmail.com>
2023-11-21 17:28:55 +0800

    add logger

--- server/src/middlewares/logger.ts
+++ server/src/middlewares/logger.ts
@@ -1,5 +1,21 @@
 import morgan from "morgan";
+import { createLogger, transports, format } from "winston";
 
 export const loggerMiddleware = morgan(
   "Request Method: :method, Request URL: :url, Response Status :status,  Response Time: :response-time ms"
 );
+
+export const logger = createLogger({
+  transports: [
+    new transports.File({
+      filename: "logs/error.log",
+      level: "error",
+      format: format.combine(format.timestamp(), format.json()),
+    }),
+    new transports.File({
+      filename: "logs/info.log",
+      level: "info",
+      format: format.combine(format.timestamp(), format.json()),
+    }),
+  ],
+});
