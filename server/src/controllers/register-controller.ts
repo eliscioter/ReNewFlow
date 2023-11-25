b1b22aa8687d7ac9110c82d3012280dc39e6ef6f
eliscioter~ <attila433lehun@gmail.com>
2023-11-25 17:42:49 +0800

    move renewal to register for simplicity

--- server/src/controllers/register-controller.ts
+++ server/src/controllers/register-controller.ts
@@ -1,12 +1,28 @@
 import asyncHandler from "express-async-handler";
-import { handleCreateRegistration } from "../data/register-data";
+import {
+  handleCreateRegistration,
+  handleFetchRegisteredPeople,
+  handleFetchSubmittedData,
+} from "../data/register-data";
 import { logger } from "../middlewares/logger";
-import { INTERNAL_SERVER_ERROR_STATUS } from "../util/http-status-codes";
+import {
+  BAD_REQUEST_STATUS,
+  INTERNAL_SERVER_ERROR_STATUS,
+  OK_STATUS,
+} from "../util/http-status-codes";
 import { validateRegisterForm } from "../validations/register-validation";
-import { FileHandlingData, RegisterForm } from "../types/validation-types";
+import {
+  FileHandlingData,
+  RegisterForm,
+  RenewForm,
+} from "../types/validation-types";
+import { handleCreateRenewal } from "../data/renewal-data";
+import { validateRenewalForm } from "../validations/renewal-validation";
+import { IDSchema } from "../validations/schema/id";
+import { fromZodError } from "zod-validation-error";
+import { prisma } from "../config/prisma";
 
-
-
+// TODO: Delete submitted files if registration fails
 export const submitRegistration = asyncHandler(async (req, res) => {
   try {
     const register_form: FileHandlingData = req.body;
@@ -38,11 +54,100 @@ export const submitRegistration = asyncHandler(async (req, res) => {
       return;
     }
 
-    res.status(create_register.code).json({ message: create_register.response });
+    res
+      .status(create_register.code)
+      .json({ message: create_register.response });
   } catch (error) {
     logger.log("error", `${error}`);
     res
       .status(INTERNAL_SERVER_ERROR_STATUS)
       .json({ message: "Server Submit Register Error" });
+  }
+});
+
+// TODO: Delete submitted files if renewal fails
+export const submitRenewal = asyncHandler(async (req, res) => {
+  try {
+    const renewal_form: FileHandlingData = req.body;
+
+    const updated_renewal_form: RenewForm = {
+      ...renewal_form.non_file_fields,
+      picture: renewal_form.file_paths[0],
+      receipt: renewal_form.file_paths[1],
+      signature: renewal_form.file_paths[2],
+      regionalCert: renewal_form.file_paths.slice(3, 4),
+      nationalCert: renewal_form.file_paths.slice(4, 5),
+    };
+
+    const validate_renewal_form = validateRenewalForm(updated_renewal_form);
+
+    if (!validate_renewal_form.valid || !validate_renewal_form.response) {
+      res
+        .status(validate_renewal_form.code)
+        .json({ message: validate_renewal_form.error });
+      return;
+    }
+
+    const create_renewal = await handleCreateRenewal(
+      validate_renewal_form.response
+    );
+
+    if (!create_renewal.success) {
+      res.status(create_renewal.code).json({ message: create_renewal.error });
+      return;
+    }
+
+    res.status(create_renewal.code).json({ message: create_renewal.response });
+  } catch (error) {
+    logger.log("error", `${error}`);
+    res
+      .status(INTERNAL_SERVER_ERROR_STATUS)
+      .json({ message: "Server Submit Renewal Error" });
+  }
+});
+
+export const fetchRegisteredPeople = asyncHandler(async (_, res) => {
+  try {
+    const fetched_data = await handleFetchRegisteredPeople();
+
+    const response = fetched_data.success
+    ? fetched_data.response
+    : fetched_data.error;
+      
+    res.status(fetched_data.code).json({ response });
+  } catch (error) {
+    logger.log("error", `${error}`);
+    res
+      .status(INTERNAL_SERVER_ERROR_STATUS)
+      .json({ message: "Server Fetch Registered People Error" });
+  }
+});
+
+export const fetchSubmittedData = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const validate_id = IDSchema.safeParse(id);
+
+    if (!validate_id.success) {
+      res
+        .status(BAD_REQUEST_STATUS)
+        .json({ message: fromZodError(validate_id.error) });
+      return;
+    }
+
+    const fetch_data = await handleFetchSubmittedData(validate_id.data);
+
+    if (!fetch_data.success) {
+      res.status(fetch_data.code).json({ message: fetch_data.error });
+      return;
+    }
+
+    res.status(fetch_data.code).json({ data: fetch_data.response });
+  } catch (error) {
+    logger.log("error", `${error}`);
+    res
+      .status(INTERNAL_SERVER_ERROR_STATUS)
+      .json({ message: "Server Fetch Submitted Data Error" });
   }
 });
--- server/src/routers/register-router.ts
+++ server/src/routers/register-router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { submitRegistration } from "../controllers/register-controller";
+import { fetchRegisteredPeople, fetchSubmittedData, submitRegistration, submitRenewal } from "../controllers/register-controller";
 import { fileHandling } from "../middlewares/busboy";
 
 export const registerRouter = Router();
 
 registerRouter.post("/submit",fileHandling, submitRegistration);
+registerRouter.post("/submit", fileHandling, submitRenewal);
+
+registerRouter.get("/fetch/registered", fetchRegisteredPeople);
+registerRouter.get("/fetch/submitted/:id", fetchSubmittedData);
+
