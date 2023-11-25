5b1b2ebc2afe0a2ff9ebc50531bea380c268f7fc
eliscioter~ <attila433lehun@gmail.com>
2023-11-25 17:42:01 +0800

    remove

--- server/src/routers/renewal-router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { submitRenewal } from "../controllers/renewal-controller";
-import { fileHandling } from "../middlewares/busboy";
-
-export const renewalRouter = Router();
-
-renewalRouter.post("/submit", fileHandling, submitRenewal);
