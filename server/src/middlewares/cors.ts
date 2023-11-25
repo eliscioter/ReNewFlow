0ce0e921728e796d6fb16957e8a42afddb3d129e
eliscioter~ <attila433lehun@gmail.com>
2023-11-25 17:45:09 +0800

    initialize client origin

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -1,8 +1,18 @@
 import cors, { CorsOptions } from "cors";
+import { config } from "dotenv";
+import { logger } from "./logger";
+
+config();
+
+if(!process.env.CLIENT_ORIGIN) {
+  logger.error("CLIENT_ORIGIN is not defined");
+  throw new Error("CLIENT_ORIGIN is not defined");
+}
 
 const cors_options: CorsOptions = {
-    credentials: true,
-    origin: process.env.CLIENT_ORIGIN
-}
+  credentials: true,
+  optionsSuccessStatus: 200,
+  origin: process.env.CLIENT_ORIGIN,
+};
 
 export const corsMiddleware = cors(cors_options);
